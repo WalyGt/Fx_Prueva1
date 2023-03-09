ee865d45cc5b0ee82ef8eb4f99c93f132fc95551
walyn <walyn@WalterAcuta>
2023-03-08 23:22:43 -0600

    no se que hice xD

---
+++ src/Controlador/EjemploVistaControl.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author walyn
+ */
+public class EjemploVistaControl implements Initializable {
+
+    @FXML
+    private Button btnBoton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void click(ActionEvent event) {
+        System.out.println("Lo Logre xD");
+    }
+   
+    
+}
